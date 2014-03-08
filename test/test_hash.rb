514a070a2f6a1af15871b073bd59ecbb21d7204d
yoshihara <h.yoshihara@everyleaf.com>
2014-03-08 18:09:34 +0900

    test: add the missing test for Hash including short Array

--- test/test_hash.rb
+++ test/test_hash.rb
@@ -45,6 +45,19 @@ ACTUAL
 EXPECTED
         end
 
+        def test_including_short_array
+          actual_string = <<ACTUAL
+{"key1":["1","2"], "key2":"value2"}
+ACTUAL
+          assert_lazy_json(<<EXPECTED, actual_string)
+{
+  "key1":
+  ["1", "2"],
+  "key2":"value2"
+}
+EXPECTED
+        end
+
         def test_including_hash
           actual_string = <<ACTUAL
 {"key1":{"key1-1":"value1-1","key1-2":"value1-2"}, "key2":"value2"}
