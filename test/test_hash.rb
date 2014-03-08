86fd1f79e5b88c3783131ba40f4fbb80306c8a1d
yoshihara <h.yoshihara@everyleaf.com>
2014-03-08 18:08:51 +0900

    test: split module or class name in Lazy::PP::JSON::HashTest

--- test/test_hash.rb
+++ test/test_hash.rb
@@ -2,36 +2,38 @@
 
 require "testutils"
 
-module Lazy::PP
-  class JSON::HashTest < Test::Unit::TestCase
-    include Lazy::PP::JSON::TestUtils
+module Lazy
+  module PP
+    class JSON
+      class HashTest < Test::Unit::TestCase
+        include Lazy::PP::JSON::TestUtils
 
-    def test_empty
-      assert_lazy_json("{}\n", '{}')
-    end
+        def test_empty
+          assert_lazy_json("{}\n", '{}')
+        end
 
-    def test_single
-      assert_lazy_json(<<EXPECTED, '{"key":"value"}')
+        def test_single
+          assert_lazy_json(<<EXPECTED, '{"key":"value"}')
 {
   "key":"value"
 }
 EXPECTED
-    end
+        end
 
-    def test_double
-      assert_lazy_json(<<EXPECTED, '{"key1":"value1", "key2":"value2"}')
+        def test_double
+          assert_lazy_json(<<EXPECTED, '{"key1":"value1", "key2":"value2"}')
 {
   "key1":"value1",
   "key2":"value2"
 }
 EXPECTED
-    end
+        end
 
-    def test_including_array
-      actual_string = <<ACTUAL
+        def test_including_array
+          actual_string = <<ACTUAL
 {"key1":["first","second"], "key2":"value2"}
 ACTUAL
-      assert_lazy_json(<<EXPECTED, actual_string)
+          assert_lazy_json(<<EXPECTED, actual_string)
 {
   "key1":
   [
@@ -41,13 +43,13 @@ ACTUAL
   "key2":"value2"
 }
 EXPECTED
-    end
+        end
 
-    def test_including_hash
-      actual_string = <<ACTUAL
+        def test_including_hash
+          actual_string = <<ACTUAL
 {"key1":{"key1-1":"value1-1","key1-2":"value1-2"}, "key2":"value2"}
 ACTUAL
-      assert_lazy_json(<<EXPECTED, actual_string)
+          assert_lazy_json(<<EXPECTED, actual_string)
 {
   "key1":
   {
@@ -57,6 +59,8 @@ ACTUAL
   "key2":"value2"
 }
 EXPECTED
+        end
+      end
     end
   end
 end
