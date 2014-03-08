dae6cd5e499db7cf69fd994ea625579be2c7046e
yoshihara <h.yoshihara@everyleaf.com>
2014-03-08 18:05:07 +0900

    test: share #assert_lazy_json

--- test/test_array.rb
+++ test/test_array.rb
@@ -100,13 +100,5 @@ ACTUAL
 ]
 EXPECTED
     end
-
-    private
-
-    def assert_lazy_json(expected, actual_string)
-      actual = Lazy::PP::JSON.new(actual_string)
-      pp(actual)
-      assert_equal(expected, @stdout.string, @stdout.string)
-    end
   end
 end
--- test/testutils.rb
+++ test/testutils.rb
@@ -16,6 +16,14 @@ module Lazy
         def teardown
           $stdout = @original_stdout
         end
+
+        private
+
+        def assert_lazy_json(expected, actual_string)
+          actual = Lazy::PP::JSON.new(actual_string)
+          pp(actual)
+          assert_equal(expected, @stdout.string, @stdout.string)
+        end
       end
     end
   end
